8a7b0719a45b277627227306b418b9d9d2f13a2a
Tiago Egas <tiagoegas@outlook.com>
2024-04-03 22:35:08 -0300

    Prisma install, init and first model Events.

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,10 @@ import fastify from "fastify";
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello NLW Unite'
+app.post('/events', () => {
+    
 })
 
-app.get('/test', () => {
-    return 'Test NLW Unite'
-})
 app.listen({port: 3333}).then(r => {
     console.log('HTTP server running...')
 })
